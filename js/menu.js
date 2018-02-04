673b77b591b9e568dfbc83d8a09480dce9368837
unknown <swm1000@hotmail.com>
2018-02-04 08:58:12 -0500

    js dropdown list

--- js/menu.js
+++ js/menu.js
@@ -2,3 +2,14 @@ var $select = $("<select></select>");
 
 $("#main").append($select);
 
+$("#main a").each(function() {
+    var $option = $("<option></option>");
+    $option.val($(this).attr("href"));
+    $option.text($(this).text());
+
+    if($(this).parent().hasClass("highlight")) {
+        $option.prop("highlight", true);
+    }
+
+    $select.append($option);
+});
