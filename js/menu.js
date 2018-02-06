55fe981dffb1d7af6317482328962f1f20254ea7
unknown <swm1000@hotmail.com>
2018-02-05 22:15:28 -0500

    change function for select var

--- js/menu.js
+++ js/menu.js
@@ -2,6 +2,10 @@ var $select = $("<select></select>");
 
 $("#main").append($select);
 
+$select.change(function() {
+    window.location = $select.val();
+});
+
 $("#main a").each(function() {
     var $option = $("<option></option>");
     $option.val($(this).attr("href"));
